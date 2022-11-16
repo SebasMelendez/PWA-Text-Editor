e788cb169e9c5e50d3697e4ed55323cfe35cf34d
Seb <samm1497@hotmail.com>
2022-11-16 00:12:47 +0000

    fixed service worker kink

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
 	return {
@@ -23,20 +21,20 @@ module.exports = () => {
 				title: 'Text Editor',
 			}),
 			new InjectManifest({
-				swSrc: './src/src-sw.js',
+				swSrc: './src-sw.js',
 				swDest: 'service-worker.js',
 			}),
 			new WebpackPwaManifest({
-				name: 'Text Editor',
-				short_name: 'Text Editor',
-				description: 'Write down code and notes.',
+				name: ' JATE: Text Editor',
+				short_name: 'J.A.T.E',
+				description: 'This is Just Another Text Editor',
 				background_color: '#1e1e1e',
 				theme_color: '#1e1e1e',
 				start_url: '/',
 				publicPath: '/',
 				icons: [
 					{
-						src: path.resolve('assets/images/logo.png'),
+						src: path.resolve('src/images/logo.png'),
 						sizes: [96, 128, 192, 256, 384, 512],
 						destination: path.join('assets', 'icons'),
 					},
@@ -48,7 +46,7 @@ module.exports = () => {
 			rules: [
 				{
 					test: /\.css$/i,
-					use: [MiniCssExtractPlugin.loader, 'css-loader'],
+					use: ['style-loader', 'css-loader'],
 				},
 				{
 					test: /\.m?js$/,
