09b0cc9ad3d06b1a43b0b90971431fb538e11558
Seb <samm1497@hotmail.com>
2022-11-16 00:28:46 +0000

    finished, set up db icon still broken :/

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+export const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({content: content});
+  const result = await request;
+  console.log('Data Saved to DV', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get('content');
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
